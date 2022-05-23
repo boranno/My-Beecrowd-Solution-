173661f10c7832e36709762cb2e0cd640af41ca7
Boranno Golder <48504390+boranno@users.noreply.github.com>
2022-05-23 08:40:22 -0700

    Update 1004.c

--- 1004.c
+++ 1004.c
@@ -3,7 +3,7 @@
 int main() {
 
      //Read two integer values. After this, calculate the product between them and store the result in a variable named PROD.
-     //Print the result like the example below.
+     //And Print the result like the example below.
     int a,b,prod;
     scanf("%d%d",&a,&b);
     prod=a*b;
