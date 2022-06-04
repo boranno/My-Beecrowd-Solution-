195bc8980cd3e57d0937d78ea3d5cc9b602bd1ac
Boranno Golder <48504390+boranno@users.noreply.github.com>
2022-06-05 00:34:40 +0600

    Add files via upload

---
+++ 1134.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int a=0,g=0,d=0,f;
+    while(1)
+    {
+        scanf("%d",&f);
+        if(f==1)
+        {
+            a++;
+        }
+        else if(f==2)
+        {
+            g++
+        }
+        else if(f==3)
+        {
+            d++;
+        }
+        else if(f==4)
+        {
+            break;
+        }
+    }
+    printf("MUITO OBRIGADO\n");
+    printf("Alcool: %d\n",a);
+    printf("Gasolina: %d\n",g);
+    printf("Diesel: %d\n",d);
+    return 0;
+}
