ce4a49d67114be367398e81feeba9150d752760e
Boranno Golder <48504390+boranno@users.noreply.github.com>
2022-06-05 00:35:50 +0600

    Add files via upload

---
+++ 1115.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    while(1)
+    {
+        int x,y;
+        scanf("%d %d",&x,&y);
+        if(x>0 && y>0)
+        {
+            printf("primeiro\n");
+        }
+        else if(x>0 && y<0)
+        {
+            printf("quarto\n");
+        }
+        else if(x<0 && y>0)
+        {
+            printf("segundo\n");
+        }
+        else if(x<0 && y<0)
+        {
+            printf("terceiro\n");
+        }
+        else if(x==0 || y==0)
+        {
+            break;
+        }
+    }
+    return 0;
+}
